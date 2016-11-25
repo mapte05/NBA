3f5c2bc455a41b99887ac937c9eb5394458321b5
Maneesh Apte <maneesh@kanjoya.com>
2016-11-25 01:21:42 -0800

    saweeet betting line odds data for 2014-15 in betting_lines.pickle file

--- nba_codebase/betting_line/scrape_betting_data.py
+++ nba_codebase/betting_line/scrape_betting_data.py
@@ -1,6 +1,7 @@
 # scrapes covers.com to get point spread data
 # usage: in terminal - "python scrape_betting_data.py"
-# desired result: outputs a dump of team matchups -> point spread betting line data from the 2014-15 NBA season
+# desired result: outputs a dump of team -> date -> opponent -> point spread 
+# 			betting line data from the 2014-15 NBA season, in file "betting_lines.pickle"
 
 import csv
 import sys
@@ -9,12 +10,74 @@ import pickle
 import requests  
 import json
 import collections
+from lxml import html
+import re
+
+
+ODD_NOT_AVAILABLE = "N/A"
+
+
+def getTeamNames(tree):
+	teamNames = tree.xpath('//div[@class="cmg_team_name"]/text()')
+	teamNamesProcessed = []
+	for name in teamNames:
+		name = name.strip()
+		if name != "":
+			teamNamesProcessed.append(name)
+	result = []
+	for i in xrange(0, len(teamNamesProcessed), 2):
+		matchup = (teamNamesProcessed[i], teamNamesProcessed[i+1])
+		result.append(matchup)
+	return result
+
+
+def getOdds(htmlData):
+	matches = re.findall("data-game-odd=\"(.*?)\"", htmlData)
+	odds = []
+	for m in matches:
+		if m == '':
+			odds.append(ODD_NOT_AVAILABLE)
+		else:
+			odds.append(m)
+	return odds
 
 
 def main():
+	# get dict with date -> html
 	with open('betting_line_html.pickle', 'rb') as handle:
 		dates_to_html = pickle.load(handle)
-	print dates_to_html
+	
+	# process html to create team -> date -> opponent -> point spread from team's perspective
+	master_point_spread = {}
+	for date in dates_to_html:
+		print date
+		htmlData = dates_to_html[date].encode('utf-8')
+		tree = html.fromstring(htmlData)
+		matchups = getTeamNames(tree) # get the team names: [team, opp-team, team, opp-team, ...]
+		odds = getOdds(htmlData) # get betting line odds via regex, N.B. some odds weren't available
+		assert len(matchups) == len(odds) # sanity check, assume that they follow the same order
+
+		# cycle through both and add to to dict
+		for i, teams in enumerate(matchups):
+			pointSpread = odds[i]
+			# first is away team, second is home team
+			# a positive point spread means home team not favored to win, negative means is favored to win
+			master_point_spread[teams[1]] = {date: ""}
+			master_point_spread[teams[1]][date] = {teams[0]: ""}
+			master_point_spread[teams[1]][date][teams[0]] = pointSpread
+			negatedPointSpread = ""
+			if pointSpread.find("-") != -1: # means the spread for home team is negative
+				negatedPointSpread = pointSpread[1:]
+			else:
+				negatedPointSpread = "-" + pointSpread
+			master_point_spread[teams[0]] = {date: ""}
+			master_point_spread[teams[0]][date] = {teams[1]: ""}
+			master_point_spread[teams[0]][date][teams[1]] = negatedPointSpread
+
+	# dump dict with point spread odds into pickle file
+	with open('betting_lines.pickle', 'wb') as handle:
+		pickle.dump(master_point_spread, handle)
+
 
 
 if __name__ == '__main__':
@@ -24,13 +87,10 @@ if __name__ == '__main__':
 
 """
 notes:
+		matchups = tree.xpath('//div[@class="cmg_matchup_header_team_names"]/text()')
+		matchups = [entry.strip() for entry in matchups]
+		print matchups
 
-current goal: pull in all html and save as files, associating date with the html
-format for date in dict is: FEB 20, 2015
-The regular season began on Tuesday, October 28, 2014
-The regular season ended on Wednesday April 15, 2015 
 
-use html.parse to get from file
-lets test this
 """
 
